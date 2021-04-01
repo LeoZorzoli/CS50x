b4f40c0ed550760362163a754812c4146518cc67
Leonel Zorzoli <63476706+LeoZorzoli@users.noreply.github.com>
2021-04-01 16:12:40 -0300

    Delete pset1 directory

--- pset1/hello/hello.c
+++
@@ -1,9 +0,0 @@
-// Libraries
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    string name = get_string("What's your name?\n"); //Variable name = Prompt
-    printf("Hello, %s\n", name); //Output
-}
